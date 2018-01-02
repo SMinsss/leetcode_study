eb8ae7d76ee38212508e994b943b180c75731384
SMinsss <2969192250@qq.com>
2018-01-02 10:46:23 +0800

    Improve the code when (m+n)%2 == 0

--- smjavalib1/src/main/java/com/sm/medianoftwoarray/Solution.java
+++ smjavalib1/src/main/java/com/sm/medianoftwoarray/Solution.java
@@ -28,9 +28,10 @@ class Solution {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length, n = nums2.length;
-        int k = (m + n) / 2;
-        if((m+n)%2==0){
-            return (findKth(nums1,nums2,0,0,m,n,k)+findKth(nums1,nums2,0,0,m,n,k+1))/2;
+        int sum = m + n;
+        int k = sum >> 1;
+        if((sum)%2==0){
+            return findKth2(nums1, nums2, 0, 0, m, n, k);
         }   else {
             return findKth(nums1,nums2,0,0,m,n,k+1);
         }
@@ -48,12 +49,58 @@ class Solution {
         }
         int p1 = Math.min(k/2,len1) ;
         int p2 = k - p1;
-        if(arr1[start1 + p1-1]<arr2[start2 + p2-1]){
+        int a1 = arr1[start1 + p1-1];
+        int a2 = arr2[start2 + p2-1];
+        int diff = a1 - a2;
+        if(diff < 0) {
             return findKth(arr1,arr2,start1 + p1,start2,len1-p1,len2,k-p1);
-        } else if(arr1[start1 + p1-1]>arr2[start2 + p2-1]){
+        } else if(diff > 0) {
             return findKth(arr1,arr2,start1,start2 + p2,len1,len2-p2,k-p2);
         } else {
             return arr1[start1 + p1-1];
+        }
+    }
+
+    private double findKth2(int[] arr1, int[] arr2, int start1, int start2, int len1, int len2, int k){
+        if(len1>len2){
+            return findKth2(arr2,arr1,start2,start1,len2,len1,k);
+        }
+        if(len1==0){
+            return ((double)arr2[start2 + k - 1] + (double)arr2[start2 + k])/2;
+        }
+        if(k==1){
+            System.out.format("k==1 , start1: %d, start2: %d, len1: %d, len2: %d\n", start1, start2, len1 ,len2);
+            double result1, result2;
+            if(arr1[start1] > arr2[start2]) {
+                result1 = arr2[start2];
+                start2++;
+            } else {
+                result1 = arr1[start1];
+                start1++;
+            }
+            if(start1 == arr1.length) {
+                result2 = arr2[start2];
+            } else if(start2 == arr2.length) {
+                result2 = arr1[start1];
+            } else if(arr1[start1] > arr2[start2]) {
+                result2 = arr2[start2];
+            } else {
+                result2 = arr1[start1];
+            }
+            System.out.format("result1: %f, result2: %f\n", result1, result2);
+            return (result1 + result2) / 2;
+        }
+        int p1 = Math.min(k/2,len1) ;
+        int p2 = k - p1;
+        int a1 = arr1[start1 + p1-1];
+        int a2 = arr2[start2 + p2-1];
+        int diff = a1 - a2;
+        if(diff < 0) {
+            System.out.format("diff < 0 , start1: %d, start2: %d, len1: %d, len2: %d\n", start1, start2, len1 ,len2);
+            return findKth2(arr1,arr2,start1 + p1,start2,len1-p1,len2,k-p1);
+        } else {
+            System.out.format("diff > 0, start1: %d, start2: %d, len1: %d, len2: %d\n", start1, start2, len1 ,len2);
+            return findKth2(arr1,arr2,start1,start2 + p2,len1,len2-p2,k-p2);
         }
     }
 
@@ -97,6 +144,13 @@ class Solution {
 
         num1Objs.add(new int[]{1,1});
         num2Objs.add(new int[]{6,6});
+
+        num1Objs.add(new int[]{1,2});
+        num2Objs.add(new int[]{1,2});
+
+        num1Objs.add(new int[]{4});
+        num2Objs.add(new int[]{1,2,3});
+
 
         Solution solution = new Solution();
         for(int i = 0; i < num1Objs.size() ;i++) {
